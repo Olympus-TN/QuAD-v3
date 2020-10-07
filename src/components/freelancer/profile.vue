d5e74b0eefecdda4d397dab5de901f086d93984d
Wassim-Nahdi <wassimnahdi@gmail.com>
2020-10-07 17:15:31 +0100

    profile all ready

--- src/components/freelancer/profile.vue
+++ src/components/freelancer/profile.vue
@@ -137,27 +137,29 @@
                     </button>
                 </div>
             <div class="modal-body">
+              <form v-on:submit.prevent="saveChanges">
                 <label for="label-name">New name :</label>
-                    <input type="text" class="form-control" id="name" placeholder="New Name" autofocus />
+                    <input type="text" class="form-control" id="name" placeholder="New Name" v-model="newUsername" autofocus />
                 <label for="label-email">New E-mail :</label>
-                    <input type="email" class="form-control" id="email" placeholder="New E-mail" autofocus />
+                    <input type="email" class="form-control" id="email" placeholder="New E-mail" v-model="newEmail" autofocus />
                 <label for="label-phone">New Number :</label>
-                    <input type="tel" class="form-control" id="phone" placeholder="+216" autofocus />
+                    <input type="tel" class="form-control" id="phone" placeholder="+216" v-model="newPhoneNumber" autofocus />
                 <div class="modal-footer">
                   <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Change Media</button>
                     <div class="row">
                       <div class="dropdown-menu">
-                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-3 icon-inline"><circle cx="12" cy="12" r="10" ></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg><input type="text" placeholder="New Website"/></h6>
-                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-3 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg><input type="text" placeholder="New Github"/></h6>
-                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-3 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg><input type="text" placeholder="Twitter"/></h6>
-                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-3 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg><input type="text" placeholder="Instagram"/></h6>
-                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-3 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg><input type="text" placeholder="Facebook"/></h6>
+                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-3 icon-inline"><circle cx="12" cy="12" r="10" ></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg><input type="text" placeholder="New Website" v-model="newWebsite"/></h6>
+                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-3 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg><input type="text" placeholder="New Github" v-model="newGithub"/></h6>
+                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-3 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg><input type="text" placeholder="Twitter" v-model="newTwitter"/></h6>
+                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram mr-3 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg><input type="text" placeholder="Instagram" v-model="newInstagram"/></h6>
+                        <h6 class="mb-0 dropdown-item"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook mr-3 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg><input type="text" placeholder="Facebook" v-model="newFacebook"/></h6>
                       </div>
                         <div class="col-lg-6 col-md-6 col-sm-12" style="padding-left: 15%; float: right">
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
+              </form>
             </div>
             </div>
         </div>
@@ -165,9 +167,38 @@
 </div>
 </template>
 <script>
+// import axios from "axios";
 export default {
-    name: "profile",
+  name: "profile",
+  data () {
+  return {
+    newUsername: "",
+    newEmail: "",
+    newPhoneNumber: "",
+    newWebsite: "",
+    newGithub: "",
+    newTwitter: "",
+    newInstagram: "",
+    newFacebook: ""
+  }
+  },
+  methods: {
+    saveChanges: function() {
+    const changing = {
+    newUsername: this.newUsername,
+    newEmail: this.newEmail,
+    newPhoneNumber: this.newPhoneNumber,
+    newWebsite: this.newWebsite,
+    newGithub: this.newGithub,
+    newTwitter: this.newTwitter,
+    newInstagram: this.newInstagram,
+    newFacebook: this.newFacebook
+  }
+  console.log('this is changing ===========> ', changing)
+  },
 }
+}
+
 </script>
 <style scoped>
 
