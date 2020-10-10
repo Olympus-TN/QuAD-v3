7901f7976fd06b428434a08590cc049e5d108139
alaa-lasoued <drakworldr8@gmail.com>
2020-10-10 13:10:45 +0100

    all issue fixed and rendering fixed with conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="appDiv">
-    <freelancerSection />
-    <freelancerHome :data="data"/>
-    <navbar  @childToParent="changeuserdata" @companydata="changecompanydata" />
-    <signup />
-    <postJob :cdata="cdata"/>
-    <profile :data="data" />
-    <companyProfile :cdata="cdata"/>
-    <companyhomepage :cdata="cdata"/>
+    <companynavbar v-if="companynavbar" @companyhome="companyhome" @companyprofil="companyProfilec" @companypostjob="companypostjobc"/>
+    <freelancernavbar v-if="freelancernavbar" @freelancerhome="homec" @freelancerprofile="profile"/>
+    <freelancerSection v-if="test"/>
+    <freelancerHome :data="data" v-if="freelancerHome"/>
+    <navbar  @childToParent="changeuserdata" @companydata="changecompanydata" v-if="landingpage"/>
+    <signup v-if="landingpage"/>
+    <postJob :cdata="cdata" v-if="companypostjob"/>
+    <profile :data="data"  v-if="freelancerprofil" />
+    <companyProfile :cdata="cdata" v-if="companyProfile"/>
+    <companyhomepage :cdata="cdata" v-if="companyhomepage"/>
+    
   </div>
 </template>
 
@@ -20,6 +23,8 @@ import profile from "./components/freelancer/profile";
 import companyProfile from "./components/company/profile.vue";
 import freelancerHome from "./components/freelancer/homePage";
 import companyhomepage from "./components/company/homePage.vue"
+import freelancernavbar from "./components/freelancer/freelancerNavbar.vue"
+import companynavbar from "./components/company/companyNavbar.vue"
 export default {
   name: "App",
   components: {
@@ -30,20 +35,90 @@ export default {
     profile,
     postJob,
     companyProfile,
-    companyhomepage
+    companyhomepage,
+    freelancernavbar,
+  companynavbar
   },
    data() {
     return {
       data : [],
-      cdata:[]
+      cdata:[],
+            landingpage:true,
+      ////Freelancer////
+      test:false,
+      freelancerHome:false,
+      freelancernavbar:false,
+      freelancerprofil:false,
+      about:false,
+      //company//
+      companyhomepage:false,
+      companyProfile:false,
+      companynavbar:false,
+      companypostjob:false
     }
     },
   methods:{
    changeuserdata: function(value){
      this.data=value
+     this.landingpage=false
+     this.freelancerHome=true
+     this.freelancernavbar=true
+     this.freelancerprofil=false
+     this.about=false
+
+    
    },
    changecompanydata: function(value){
-     this.cdata=value
+     this.cdata=value 
+     this.landingpage=false
+     this.companynavbar=true
+     this.companyhomepage=true
+   },
+   homec:function(value){
+    if(value===true){
+      this.freelancerHome=true
+      this.freelancerprofil=false
+         this.about=false
+    }
+   },
+   profile:function(value){
+     if(value===true){
+       this.freelancerHome=false
+       this.freelancerprofil=true
+          this.about=false
+     }
+   },
+   aboutc:function(value){
+     if(value===true){
+       this.freelancerHome=false
+       this.freelancerprofil=false
+       this.about=true
+       
+     }
+   },
+   companyhome:function(value){
+     if(value===true){
+       this.companyhomepage=true
+       this.about=false
+       this.companyProfile=false
+       this.companypostjob=false
+     }
+   },
+   companyProfilec:function(value){
+     if(value===true){
+       this.companypostjob=false
+       this.companyhomepage=false
+       this.about=false
+       this.companyProfile=true
+     }
+   },
+   companypostjobc:function(value){
+     if(value===true){
+       this.companyProfile=false
+       this.about=false
+       this.companypostjob=true
+       this.companyhomepage=false
+     }
    }
   }
 };
--- src/components/freelancer/freelancerSection.vue
+++ src/components/freelancer/freelancerSection.vue
@@ -1,23 +1,27 @@
 
 <template>
   <div>
-    <router-view>
-      <navbar />
-      
-    </router-view>
+    <!-- <p>
+      <router-link to="/home" class="link">HomePage</router-link>
+      <router-link to="/profile" class="link">Profile</router-link>
+      <router-link to="/newJob" class="link">Post a new Job</router-link>
+    </p> -->
+    <!-- <router-view></router-view> -->
+    <homePage />
+    <profile />
   </div>
 </template>
 
 <script>
-import navbar from "./freelancerNavbar"
-// import homePage from "../freelancer/homePage";
-// import profile from "../freelancer/profile";
+// import navbar from "./freelancerNavbar"
+import homePage from "../freelancer/homePage";
+import profile from "../freelancer/profile";
 export default {
   name: "freLaSec",
   components: {
-    navbar,
-    // homePage,
-    // profile,
+    // navbar,
+    homePage,
+    profile,
   },
 };
 </script>
